850827fd3591487eed96bb0a09ea419eed1e6ce3
Sam Lynch <samdawkins1998@gmail.com>
2021-12-08 10:05:35 -0500

    new js

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 
-let weather = {
+const weather = {
     apiKey: "4cd6d52c9d7fffcfa793f2c851a64410",
     fetchWeather: function (city) {
       fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-          city +
-          "&units=metric&appid=" +
-          this.apiKey
-      )
+        "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey)
         .then((response) => {
           if (!response.ok) {
             alert("No weather found.");
@@ -17,16 +13,17 @@ let weather = {
         })
         .then((data) => this.displayWeather(data));
     },
+    
     displayWeather: function (data) {
-      const { name } = data;
-      const { icon, description } = data.weather[0];
-      const { temp, humidity } = data.main;
-      const { speed } = data.wind;
+      const {name} = data;
+      const {icon, description} = data.weather[0];
+      const {temp, humidity} = data.main;
+      const {speed} = data.wind;
       document.querySelector(".city").innerText = "Weather in " + name;
       document.querySelector(".icon").src =
         "https://openweathermap.org/img/wn/" + icon + ".png";
       document.querySelector(".description").innerText = description;
-      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".temp").innerText = temp + "°F";
       document.querySelector(".humidity").innerText =
         "Humidity: " + humidity + "%";
       document.querySelector(".wind").innerText =
@@ -48,3 +45,4 @@ let weather = {
     });
   
   weather.fetchWeather("Columbia");
+
